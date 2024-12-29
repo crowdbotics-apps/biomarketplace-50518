dde84281e0df2c1059d034994ab01f2f9d5c6951
Crowdbotics <team@crowdbotics.com>
2024-12-29 18:45:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "biomarketplace-50518",
+  url: "https://biomarketplace-50518.botics.co",
+  api: "https://biomarketplace-50518.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
